a3707ae30571e22e00004aaee33551ac65c1eb9c
Andrii Chernikov <99753757+andchermeister@users.noreply.github.com>
2025-11-16 21:09:30 +0000

    Added logic fixes

--- memory-card-react-app/src/components/cards.jsx
+++ memory-card-react-app/src/components/cards.jsx
@@ -80,23 +80,41 @@ export default function Cards({ score, setScore, setHighestScore }) {
     setCards(shuffled);
   };
 
+  const checkWin = () => {
+    if (score + 1 === cards.length) {
+      alert("Congratulations, you won!");
+      restart();
+      setHighestScore((prev) => prev + 1);
+    }
+  };
+
+  const continuePlaying = (key) => {
+    setScore((prev) => prev + 1);
+    checkWin();
+    setClickedCards((prev) => ({ ...prev, [key]: true }));
+    shuffleCards();
+  };
+
+  const restart = () => {
+    setHighestScore((prev) => Math.max(prev, score));
+    setScore(0);
+    setClickedCards({});
+    shuffleCards();
+  };
+
   const handleClick = (key) => {
     if (clickedCards[key]) {
-      setHighestScore(score);
-      setScore(0);
       alert("You lost, try again!");
-      shuffleCards();
+      restart();
     } else {
-      setScore(score + 1);
-      setClickedCards((prev) => ({ ...prev, [key]: true }));
-      shuffleCards();
+      continuePlaying(key);
     }
   };
 
   return (
     <div>
       {cards.map((card) => (
-        <button onClick={() => handleClick(card.card_id)}>
+        <button key={card.card_id} onClick={() => handleClick(card.card_id)}>
           <img className="card" src={card.url} alt={card.name} />
         </button>
       ))}
